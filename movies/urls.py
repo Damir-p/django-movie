9f0876ed20ea2d4abf5e8af072a7efd20af6ba83
Damir <damir.ptn@gmail.com>
2023-01-26 14:28:34 +0600

    new_temlates

--- movies/urls.py
+++ movies/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.MoviesView.as_view()),
+    path("<slug:slug >/", views.MovieDetailViews.as_view())
 ]
--- movies/views.py
+++ movies/views.py
@@ -7,5 +7,12 @@ class MoviesView(View):
     """Список фильмов"""
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request, "movies/movie_list.html", {"movie_list": movies})
+        return render(request, "movies/movies.html", {"movie_list": movies})
     
+
+class MovieDetailViews(View):
+    """Полное описания фильма"""
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        return render(request, "movies/movie_detail.html", {"movie": movie})
+     
